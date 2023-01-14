c6d0338424fe39f1458a87994d14f590d088f101
teamgram.io <wubenqi@gmail.com>
2023-01-14 19:56:14 +0800

    feat: add pipeline in kv

--- pkg/stores/kv/examples/main.go
+++ pkg/stores/kv/examples/main.go
@@ -22,9 +22,41 @@ func main() {
 	var c Config
 	conf.MustLoad("./kv.yaml", &c)
 
-	kv := kv.NewStore(c.KV)
-	vals, _ := kv.MgetCtx(context.Background(), "document_1572598612301451270", "document_1572599736324591618")
+	dIdList := []string{
+		"document_1572598612301451270",
+		"document_1572599736324591618",
+	}
+
+	kv2 := kv.NewStore(c.KV)
+
+	// MgetCtx
+	vals, _ := kv2.MgetCtx(context.Background(), dIdList...)
 	for _, v := range vals {
 		fmt.Println(v)
+	}
+
+	// pipeline
+	pipes := make(map[interface{}][]string)
+	for _, dId := range dIdList {
+		pipe, _ := kv2.GetPipeline(dId)
+		pipes[pipe] = append(pipes[pipe], dId)
+	}
+
+	for pipe, idList := range pipes {
+		var (
+			cmds = make([]*kv.StringCmd, len(idList))
+		)
+
+		pipe.(kv.Pipeline).PipelinedCtx(context.Background(),
+			func(pipe kv.Pipeliner) error {
+				for i, id := range idList {
+					cmds[i] = pipe.Get(context.Background(), id)
+				}
+				return nil
+			})
+
+		for _, cmd := range cmds {
+			fmt.Println(cmd.Result())
+		}
 	}
 }
--- pkg/stores/kv/kv.go
+++ pkg/stores/kv/kv.go
@@ -8,10 +8,20 @@ package kv
 
 import (
 	"github.com/zeromicro/go-zero/core/stores/kv"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 )
 
 type (
-	KvConf = kv.KvConf
+	KvConf    = kv.KvConf
+	Pipeline  = kv.Pipeline
+	Pipeliner = kv.Pipeliner
+
+	// IntCmd is an alias of redis.IntCmd.
+	IntCmd = redis.IntCmd
+	// FloatCmd is an alias of redis.FloatCmd.
+	FloatCmd = redis.FloatCmd
+	// StringCmd is an alias of redis.StringCmd.
+	StringCmd = redis.StringCmd
 )
 
 var (
