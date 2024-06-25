5d0c93650a439a4032e8632953025bd4cad03b80
teamgram.io <wubenqi@gmail.com>
2024-06-25 10:43:25 +0800

    feat: disable reflection

--- pkg/container2/slice.go
+++ pkg/container2/slice.go
@@ -1,43 +1,71 @@
-// Copyright 2022 Teamgram Authors
-//  All rights reserved.
+// Copyright © 2024 Teamgram Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// Author: teamgramio (teamgram.io@gmail.com)
-//
 
 package container2
 
-import (
-	"fmt"
-	"reflect"
-)
-
-func Contains(obj interface{}, target interface{}) (bool, error) {
-	targetValue := reflect.ValueOf(target)
-	switch reflect.TypeOf(target).Kind() {
-	case reflect.Slice, reflect.Array:
-		for i := 0; i < targetValue.Len(); i++ {
-			if targetValue.Index(i).Interface() == obj {
-				return true, nil
-			}
-		}
-	case reflect.Map:
-		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
-			return true, nil
+func ContainsInt(slice []int, target int) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
 		}
 	}
-	return false, fmt.Errorf("not in")
+	return false
+}
+
+func ContainsInt32(slice []int32, target int32) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsInt64(slice []int64, target int64) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsUint32(slice []uint32, target uint32) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsUint64(slice []uint64, target uint64) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsString(slice []string, target string) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
 }
 
 func AppendIgnoreNil(slice []interface{}, elems ...interface{}) []interface{} {
